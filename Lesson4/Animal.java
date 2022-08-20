6d18bb3325796a2749624d90727a252782d2c43d
Andrii Shumak <the.mstr.anderson@gmail.com>
2022-08-20 16:17:50 +0300

    lesson4's changes for approving

---
+++ Lesson4/Animal.java
@@ -0,0 +1,15 @@
+//Class for Task6
+
+package Lesson4;
+
+public class Animal {
+    String name;
+    int age;
+    int weight;
+
+    public Animal(String name, int age, int weight) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+    }
+}
---
+++ Lesson4/Task9.java
@@ -0,0 +1,15 @@
+package Lesson4;
+
+public class Task9 {
+    public static void main(String[] args) {
+        int result = sum(10);
+        System.out.println(result);
+    }
+    public static int sum(int k) {
+        if (k > 0) {
+            return k + sum(k - 1);
+        } else {
+            return 0;
+        }
+    }
+}
